3eb85ce85f40325742f39ff207cc08b8c53c610f
Anil Pal <anilcodes01@gmail.com>
2025-01-05 20:59:36 +0530

    fixing vercel errors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,10 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   build: {
     outDir: 'dist',
-    sourcemap: true,  
     rollupOptions: {
       output: {
         manualChunks(id) {
@@ -23,10 +16,6 @@ export default defineConfig({
     }
   },
   server: {
-    port: 5173,
-    strictPort: true
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
+    port: 5173
   }
 })
